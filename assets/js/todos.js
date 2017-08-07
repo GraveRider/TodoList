30298bfa1a9080e62ecc4f17a9c12d2cae7f2b55
GraveRider <donprayer@gmail.com>
2017-08-07 11:04:13 +0300

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,25 @@
+var ul = $("ul");
+var input = $("input");
+
+ul.on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+ul.on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+input.keypress(function(event) {
+    if (event.which === 13 && input.val() !== "") {
+        var todo = $(this).val();
+        ul.append("<li><span><i class='fa fa-trash'></i></span> " + todo + "</li>");
+        input.val("");
+    }
+});
+
+$(".fa-plus").click(function() {
+    input.fadeToggle();
+});
